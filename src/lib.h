21526d69c238b663f0bac8690ec1e27e53b760b1
Myridia <info@myridia.com>
2026-04-20 12:11:32 +0700

    Update lib.h

--- src/lib.h
+++ src/lib.h
@@ -3,27 +3,29 @@
 
 #include <ctype.h>
 #include <curl/curl.h>
-#include <dirent.h>
 #include <errno.h>
 #include <jansson.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <time.h>
-#include <unistd.h>
 
 #ifdef _WIN32
 #include <direct.h>
 #include <io.h>
+#include <sys/types.h>
 #define access _access
 #define F_OK 0
 #define mkdir(path, mode) _mkdir(path)
 #else
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 #endif
+
+#endif // LIB_H
 
 #define MAX_LINE_LENGTH 256
 #define MAX_MESSAGE_ID_LENGTH 64
